4483fa54f9b8464e0ca9a3e017207d972502f78a
Bryan Ferreira <bryandfh@gmail.com>
2019-03-18 17:36:53 +0100

    "Multiple item creation"

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,9 +3,23 @@
 console.log('App.js is running');
 
 var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX from app!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Bryan Ferreira'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 23'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Santander'
+    )
 );
 var appRoot = document.getElementById('app');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,12 @@
 console.log('App.js is running')
 
-var template = <h1>This is JSX from app!</h1>
+var template = (
+    <div>
+        <h1>Bryan Ferreira</h1>
+        <p>Age: 23</p>
+        <p>Location: Santander</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot)
