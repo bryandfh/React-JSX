edce451bd1977394c088844b227ed81fb27a2e2f
Bryan Ferreira <bryandfh@gmail.com>
2019-03-19 17:20:19 +0100

    Adding variable to button

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,30 +1,87 @@
 'use strict';
 
-var user = {
-    name: 'Bryan',
-    cities: ['Caracas', 'Los Angeles', 'Santander'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
+console.log('App.js is running!');
 
-        //Other way to functions      
-        return this.cities.map(function (city) {
-            return _this.name + " lived in " + city;
-        });
-        //return cityMessage;
-    }
+var app = {
+  title: 'Indecision App ?',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
 
-console.log(user.printPlacesLived());
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
 
-var multiplier = {
-    numbers: [2, 3, 4, 5],
-    multiplyBy: 10,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (n) {
-            return _this2.multiplyBy * n;
-        });
-    }
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
 };
-console.log(multiplier.multiply());
+
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+
+var reset = function reset() {
+  console.log('reset');
+};
+
+var someId = 'myidhere';
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'Seteup reset'
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
