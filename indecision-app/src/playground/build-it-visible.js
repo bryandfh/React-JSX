ff136466dc915ac67c965b71660fb21219c29f2c
Bryan Ferreira <bryandfh@gmail.com>
2019-03-20 15:25:33 +0100

    Visibility challenge

---
+++ indecision-app/src/playground/build-it-visible.js
@@ -0,0 +1,26 @@
+const appRoot = document.getElementById('app');
+
+let app = {
+    title: 'Visibility Toggle',
+    options: 'Hey some text',
+};
+
+let flags = false;
+
+let showButton = () => {
+    flags ? flags = false : flags = true;
+    render();
+}
+
+const render = () => {
+    let template = (
+      <div>
+        <h1>{app.title}</h1>
+        <button onClick = {showButton}> {flags ? "Hide" : "Show"} </button>
+        {flags && <p>{app.options}</p>}        
+      </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+  
+render();
