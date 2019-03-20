ba35ee9b0dd369228c79cd0486547fff6f9bbf6f
Bryan Ferreira <bryandfh@gmail.com>
2019-03-20 15:25:58 +0100

    "Visibility challenge all files"

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,38 +1,17 @@
 'use strict';
 
-console.log('App.js is running!');
-
-var app = {
-  title: 'Indecision',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-};
-
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    console.log(app.options);
-    render();
-  }
-};
-
-var onRemoveAll = function onRemoveAll(e) {
-  app.options = [];
-  render();
-};
-
 var appRoot = document.getElementById('app');
 
-var number = [55, 101, 1000];
+var app = {
+  title: 'Visibility Toggle',
+  options: 'Hey some text'
+};
 
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  console.log(option);
+var flags = false;
+
+var showButton = function showButton() {
+  flags ? flags = false : flags = true;
+  render();
 };
 
 var render = function render() {
@@ -44,52 +23,20 @@ var render = function render() {
       null,
       app.title
     ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      'p',
-      null,
-      app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
     React.createElement(
       'button',
-      { disabled: app.options.length == 0, onClick: onMakeDecision },
-      'What should I do?'
+      { onClick: showButton },
+      ' ',
+      flags ? "Hide" : "Show",
+      ' '
     ),
-    React.createElement(
-      'button',
-      { onClick: onRemoveAll },
-      'Reset '
-    ),
-    React.createElement(
-      'ol',
+    flags && React.createElement(
+      'p',
       null,
-      app.options.map(function (options) {
-        return React.createElement(
-          'li',
-          { key: options },
-          'Option: ',
-          options
-        );
-      })
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option' }),
-      React.createElement(
-        'button',
-        null,
-        'Add option'
-      )
+      app.options
     )
   );
-
   ReactDOM.render(template, appRoot);
-  0;
 };
 
 render();
