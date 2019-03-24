2e5e30c206f4e124fccf036e8a2e3bb6d8b3b98b
Bryan Ferreira <bryandfh@gmail.com>
2019-03-24 21:49:49 +0100

    State for components in various functions

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,3 +1,53 @@
+class Counter extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleOne = this.handleOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count : 0
+    }
+  }
+  
+  handleOne(){
+    this.setState((pre) => {
+      return {
+        count : pre.count +1
+      };
+    });
+  }
+
+  handleMinusOne(){
+    this.setState((pre) => {
+      return {
+        count : pre.count - 1
+      };
+    });
+  }
+
+  handleReset(){
+    this.setState((pre) => {
+      return {
+        count : 0
+      };
+    });
+  };
+  
+  render(){
+    return (
+      <div>
+        <h1>Count:{this.state.count}</h1>
+        <button onClick = {this.handleOne}>+1</button>
+        <button onClick = {this.handleMinusOne}>-1</button>
+        <button onClick = {this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('app'))
+
+/*
 let count = 0;
 const addOne = () =>{
   count += 1;
@@ -33,3 +83,4 @@ const rendeCounterApp = () => {
 };
 
 rendeCounterApp();
+*/
