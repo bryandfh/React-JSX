05381795ff5ae386701320926083c44ce80d46bb
Bryan Ferreira <bryandfh@gmail.com>
2019-04-02 23:45:15 +0200

    Remove option , passing props to multiple layers

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,6 +4,7 @@ class IndecisionApp extends React.Component {
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
     this.state = {
       options: props.options
     };
@@ -12,11 +13,18 @@ class IndecisionApp extends React.Component {
     this.setState(() => ({ options : []}))
   }
 
+  handleDeleteOption(optionToRemove){
+    this.setState((prevState) => ({ 
+      options : prevState.options.filter((option) => optionToRemove !== option)
+    }))
+  }
+
   handlePick() {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
+
   handleAddOption(option) {
     if (!option) {
       return 'Enter valid value to add item';
@@ -42,6 +50,7 @@ class IndecisionApp extends React.Component {
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption
           handleAddOption={this.handleAddOption}
@@ -87,7 +96,13 @@ const Options = (props) => {
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
       {
-        props.options.map((option) => <Option key={option} optionText={option} />)
+        props.options.map((option) => (
+          <Option 
+            key={option} 
+            optionText={option}
+            handleDeleteOption={props.handleDeleteOption}
+          />)
+        )
       }
     </div>
   );
@@ -97,6 +112,9 @@ const Option = (props) => {
   return (
     <div>
       {props.optionText}
+      <button onClick={(e) => {
+        props.handleDeleteOption(props.optionText)
+      }}>Remove </button>
     </div>
   );
 };
